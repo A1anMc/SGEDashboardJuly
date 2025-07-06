8dc6c98692d4e1f6eeaa9d2acfe76f7dad6a4348
Alan McCarthy <alanmccarthy00@gmail.com>
2025-07-07 02:09:11 +1000

    Fix: Replace @/lib/utils with relative imports to resolve build issues

--- frontend/src/components/ui/command.tsx
+++ frontend/src/components/ui/command.tsx
@@ -3,7 +3,7 @@
 import * as React from 'react';
 import { DialogProps } from '@radix-ui/react-dialog';
 import { Command as CommandPrimitive } from 'cmdk';
-import { cn } from '@/lib/utils';
+import { cn } from '../../lib/utils';
 
 const Command = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive>,
